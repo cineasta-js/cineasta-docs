d0e2b37ce6aba42749aa4d7516111a84cae6da17
lucasfs7 <lucasfs7@gmail.com>
2017-02-22 19:41:25 -0300

    remove test

--- src/takes/Generating.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { component as GeneratingScene } from 'takes/Generating'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<GeneratingScene />, div)
-})
