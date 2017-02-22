6de8877566401c64d5603360f17beebecbb55e00
lucasfs7 <lucasfs7@gmail.com>
2017-02-22 19:06:00 -0300

    remove unecessary takes

--- src/takes/How.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { component as HowScene } from 'takes/How'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<HowScene />, div)
-})
--- src/takes/What.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { component as WhatScene } from 'takes/What'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<WhatScene />, div)
-})
